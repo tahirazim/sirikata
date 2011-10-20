tahirazim/sirikata

commit 6eaf58dff4c6f4c61bee5a313df7faefb4ece5c2

Implement OHDP::Service in Server and convert TimeSyncServer to use it.

libcore/src/sync/TimeSyncClient.cpp
@@ -65,6 +65,8 @@ TimeSyncClient::~TimeSyncClient() {
 }
 
 void TimeSyncClient::poll() {
+    SILOG(timesync, detailed, "Sending time sync message");
+
     // Send the next update request
     Sirikata::Protocol::TimeSync sync_msg;
     uint8 seqno = mSeqno++;
@@ -76,6 +78,8 @@ void TimeSyncClient::poll() {
 }
 
 void TimeSyncClient::handleSyncMessage(const ODP::Endpoint &src, const ODP::Endpoint &dst, MemoryReference payload) {
+    SILOG(timesync, detailed, "Received time sync reply");
+
     Sirikata::Protocol::TimeSync sync_msg;
     bool parse_success = sync_msg.ParseFromArray(payload.data(), payload.size());
     if (!parse_success) {

libcore/src/sync/TimeSyncServer.cpp
@@ -35,30 +35,45 @@
 
 #include <Protocol_TimeSync.pbj.hpp>
 #include <sirikata/core/network/Message.hpp> //serializePBJMessage
+#include <sirikata/core/network/ObjectMessage.hpp> // OBJECT_PORT_TIMESYNC
+
 namespace Sirikata {
 
-TimeSyncServer::TimeSyncServer(Context* ctx)
+TimeSyncServer::TimeSyncServer(Context* ctx, OHDP::Service* ohdp)
  : mContext(ctx)
 {
+    mPort = ohdp->bindOHDPPort(OBJECT_PORT_TIMESYNC);
+    if (mPort == NULL) {
+        SILOG(timesync,fatal,"Couldn't bind port " << OBJECT_PORT_TIMESYNC << " for TimeSyncServer, must already be in use...");
+    }
+    else {
+        SILOG(timesync, detailed, "Listening for time sync messages...");
+        mPort->receive(
+            std::tr1::bind(&TimeSyncServer::handleMessage, this, _1, _2, _3)
+        );
+    }
 }
 
 TimeSyncServer::~TimeSyncServer() {
+    delete mPort;
 }
 
-String TimeSyncServer::getResponse(MemoryReference payload) {
+void TimeSyncServer::handleMessage(const OHDP::Endpoint& src, const OHDP::Endpoint& dst, MemoryReference payload) {
+    SILOG(timesync, detailed, "Received time sync message from remote node " << src.node());
+
     Sirikata::Protocol::TimeSync sync_msg;
     bool parse_success = sync_msg.ParseFromArray(payload.data(), payload.size());
     if (!parse_success) {
         LOG_INVALID_MESSAGE_BUFFER(sync, error, ((char*)payload.data()), payload.size());
-        return "";
+        return;
     }
 
     // Our only job is to take the existing message, fill in a timestamp, and
     // send it back.
     sync_msg.set_t(mContext->simTime());
-
     String resp = serializePBJMessage(sync_msg);
-    return resp;
+    mPort->send(src, MemoryReference(payload));
+    SILOG(timesync, detailed, "Sent time sync message reply to remote node " << src.node());
 }
 
 } // namespace Sirikata

libcore/include/sirikata/core/ohdp/Defs.hpp
@@ -12,7 +12,7 @@ namespace OHDP {
 
 typedef Sirikata::XDP::PortID PortID;
 
-class NodeID {
+class SIRIKATA_EXPORT NodeID {
 public:
     NodeID();
     NodeID(uint32 rhs);
@@ -20,6 +20,10 @@ public:
 
     /** Get a null NodeID. Equivalent to NodeID(0). */
     static const NodeID& null();
+    /** Get a self NodeID, the one used for the local endpoint. Equivalent to
+     *  null, NodeID(0).
+     */
+    static const NodeID& self();
     /** Get a NodeID that matches any other NodeID. */
     static const NodeID& any();
 

libcore/include/sirikata/core/ohdp/Service.hpp
@@ -39,6 +39,22 @@ public:
      */
     virtual Port* bindOHDPPort(const SpaceID& space, const NodeID& node, PortID port) = 0;
 
+    /** Convenience form of OHDP port binding since only NodeID::self() is ever
+     *  valid for binding a Port (the local endpoint in OHDP is always
+     *  NodeID::self() i.e. 0).
+     */
+    Port* bindOHDPPort(const SpaceID& space, PortID port) {
+        return bindOHDPPort(space, NodeID::self(), port);
+    }
+
+    /** Convenience form of OHDP port binding for when the SpaceID and NodeID
+     *  are ignored because there is only one choice (e.g. on the space, where
+     *  SpaceID is always null and NodeID is always self).
+     */
+    Port* bindOHDPPort(PortID port) {
+        return bindOHDPPort(SpaceID::null(), NodeID::self(), port);
+    }
+
     /** Bind a random, unused OHDP port for use.
      *  \param space the Space to communicate via
      *  \returns an OHDP Port object which can be used immediately, or, in

libcore/include/sirikata/core/sync/TimeSyncServer.hpp
@@ -34,7 +34,7 @@
 #define _SIRIKATA_CORE_SYNC_TIME_SYNC_SERVER_HPP_
 
 #include <sirikata/core/service/Context.hpp>
-#include <sirikata/core/odp/Service.hpp>
+#include <sirikata/core/ohdp/Service.hpp>
 
 namespace Sirikata {
 
@@ -45,12 +45,13 @@ namespace Sirikata {
  */
 class SIRIKATA_EXPORT TimeSyncServer {
 public:
-    TimeSyncServer(Context* ctx);
+    TimeSyncServer(Context* ctx, OHDP::Service* ohdp);
     ~TimeSyncServer();
-
-    String getResponse(MemoryReference payload);
 private:
+    void handleMessage(const OHDP::Endpoint& src, const OHDP::Endpoint& dst, MemoryReference payload);
+
     Context* mContext;
+    OHDP::Port* mPort;
 }; // class TimeSyncServer
 
 } // namespace Sirikata

libcore/src/ohdp/Defs.cpp
@@ -31,6 +31,10 @@ const NodeID& NodeID::null() {
     return null_port;
 }
 
+const NodeID& NodeID::self() {
+    return null();
+}
+
 const NodeID& NodeID::any() {
     static NodeID any_port(ANY_NODE_ID);
     return any_port;

liboh/src/SessionManager.cpp
@@ -842,7 +842,7 @@ void SessionManager::handleSpaceConnection(const Sirikata::Network::Stream::Conn
             // The object IDs and ports used are bogus since the space server just
             // ignores them
             mTimeSyncClient = new TimeSyncClient(
-                mContext, this->bindODPPort(mSpace, ObjectReference(UUID::random()), OBJECT_PORT_TIMESYNC),
+                mContext, this->bindODPPort(mSpace, ObjectReference(UUID::null()), OBJECT_PORT_TIMESYNC),
                 ODP::Endpoint(mSpace, ObjectReference::spaceServiceID(), OBJECT_PORT_TIMESYNC),
                 Duration::seconds(5),
                 std::tr1::bind(&SessionManager::timeSyncUpdated, this)

libspace/include/sirikata/space/ObjectHostConnectionID.hpp
@@ -12,6 +12,11 @@ namespace Sirikata {
 class ObjectHostConnection;
 class ObjectHostConnectionManager;
 
+// Short version of ObjectHostConnectionID. This requires a map lookup to
+// actually send data, but is useful as a non-opaque integral type for use as an
+// enpoint identifier in protocols.
+typedef uint32 ShortObjectHostConnectionID;
+
 /** Unique identifier for an object host connected to this space server. It is
  *  only valid on this server, is opaque, and *will* change, even if the same
  *  object host connects again.
@@ -29,6 +34,9 @@ public:
 
     bool operator==(const ObjectHostConnectionID& rhs) const;
     bool operator!=(const ObjectHostConnectionID& rhs) const;
+
+    // Implementation in ObjectHostConnectionManager.cpp
+    ShortObjectHostConnectionID shortID() const;
 private:
     friend class ObjectHostConnectionManager;
 

commit 43b2e5bc302da1918541c24798e1307e2641dbef

Fix typo bug that caused time sync to fail.
Fix typo bug, introduced in 6eaf58dff, in TimeSyncServer where the
wrong message (the version before the server's timestamp was added)
was sent back to the client, causing a completely incorrect offset to
be computed.

libcore/src/sync/TimeSyncClient.cpp
@@ -113,11 +113,14 @@ void TimeSyncClient::handleSyncMessage(const OHDP::Endpoint &src, const OHDP::En
     static Duration sMaxRTT = Duration::seconds(5); // this could definitely be lower
     Duration rtt = local_finish_t - local_start_t;
     if (local_finish_t < local_start_t ||
-        rtt > sMaxRTT)
+        rtt > sMaxRTT) {
+        SILOG(timesync, detailed, "Ignoring time sync reply: out of date or non-sensical results");
         return;
+    }
 
     // FIXME use averaging, falloff, etc instead of just replacing the value outright
     mOffset = server_t - (local_start_t + (rtt/2.f));
+    SILOG(timesync, detailed, "RTT " << rtt << " updates offset to " << mOffset);
     mHasBeenInitialized = true;
     if (mCB)
         mCB();

libcore/src/sync/TimeSyncServer.cpp
@@ -72,7 +72,7 @@ void TimeSyncServer::handleMessage(const OHDP::Endpoint& src, const OHDP::Endpoi
     // send it back.
     sync_msg.set_t(mContext->simTime());
     String resp = serializePBJMessage(sync_msg);
-    mPort->send(src, MemoryReference(payload));
+    mPort->send(src, MemoryReference(resp));
     SILOG(timesync, detailed, "Sent time sync message reply to remote node " << src.node());
 }
 